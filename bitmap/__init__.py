bb296eaf595a8b216ddcfc14b6d902177d42aec7
Peter Zuidema <peter@icebear.net>
2020-01-23 23:06:34 +0100

    Initial commit

---
+++ bitmap/__init__.py
@@ -0,0 +1,25 @@
+from snake.scale import FileType, scale
+
+
+NAME = "bitmap"
+VERSION = "0.1"
+
+AUTHOR = "Peter Zuidema"
+AUTHOR_EMAIL = "peter@icebear.net"
+
+DESCRIPTION = "a module to create a bitmap of the malware sample"
+
+LICENSE = "https://github.com/srcr/bitmap-scale/blob/master/bitmap-scale/LICENSE"
+
+URL = "https://github.com/srcr/bitmap-scale"
+
+
+__scale__ = scale(
+    name=NAME,
+    description=DESCRIPTION,
+    version=VERSION,
+    author=AUTHOR,
+    supports=[
+        FileType.FILE
+    ],
+)
---
+++ bitmap/command.py
@@ -0,0 +1,26 @@
+from snake import scale
+import numpy as np
+import base64
+import math
+import cv2
+
+
+class Commands(scale.Commands):
+    def check(self):
+        pass
+
+    @scale.command({
+        'info': 'create bitmap image from the file passed'
+    })
+    def bitmap(self, args, file, opts):
+        width = 64
+        resize = 3
+        binary = np.fromfile(file.file_path, dtype='uint8')
+		lenght = len(binary)
+		height = math.ceil(length / width)
+		reshaped = np.resize(binary,(height, width))
+		dimension = (width * resize, height * resize)
+        resized = cv2.resize(reshaped, dimension, interpolation = cv2.INTER_AREA)
+        image = cv2.imencode('.png', resized)
+        encoded = base64.b64encode(image[1])
+        return {'bitmap': str('![Bitmap](data:image/png;base64,' + encoded.decode('ascii') + ')', encoding='latin-1')}
